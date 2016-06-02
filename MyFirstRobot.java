577f87da5733189e2c4a2a90e6579ea901dd749d
makaylapalmer27 <makaylapalmer27@gmail.com>
2016-06-01 19:43:04 -0500

    checkin

---
+++ MyFirstRobot.java
@@ -0,0 +1,59 @@
+package mp;
+import robocode.*;
+import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * MyFirstRobot - a robot by (your name here)
+ */
+public class MyFirstRobot extends Robot
+{
+	/**
+	 * run: MyFirstRobot's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		setColors(Color.magenta,new Color(104,250,7),new Color(0,255,229)); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+	
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(4);
+	}
+	public void onBulletMissed (BulletMissedEvent e){
+		setColors(Color.red,Color.red,Color.red);
+		
+	}
+	public void onBulletHit (BulletHitEvent e){
+		setColors(Color.green,Color.magenta,Color.green);
+	}
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
